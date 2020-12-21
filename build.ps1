666c03751bc29728cabf30315c899d83356b7a76
Jakub Čábera <cabera.jakub@gmail.com>
2020-12-21 09:44:11 -0800

    refactor: Switch to Github CI

--- build.ps1
+++ build.ps1
@@ -1,20 +1,37 @@
-Push-Location $psscriptroot
+Push-Location $PSScriptRoot
 
 # Prepare
+$csc = "$PSScriptRoot\packages\Microsoft.Net.Compilers\tools\csc.exe"
 $build = "$PSScriptRoot\build"
+$dist = "$PSScriptRoot\dist"
 $src = "$PSScriptRoot\src"
-New-Item -ItemType Directory -Path $build -ErrorAction SilentlyContinue | Out-Null
-Remove-Item "$build\*" -Recurse -Force | Out-Null
+New-Item -Path $build, $dist -ItemType 'Directory' -ErrorAction 'SilentlyContinue' | Out-Null
+Remove-Item "$build\*", "$dist\*" -Recurse -Force
+
+if ((Get-ChildItem "$PSScriptRoot\packages" -Recurse).Count -eq 0) {
+    Write-Host 'Dependencies are missing. Run ''install.ps1''' -ForegroundColor 'DarkRed'
+    exit 258
+}
 
 # Build
 Write-Output 'Compiling shim.cs ...'
-& "$PSScriptRoot\packages\Microsoft.Net.Compilers\tools\csc.exe" /deterministic /platform:anycpu /nologo /optimize /target:exe /out:"$build\shim.exe" "$src\shim.cs"
+$shimCS = @(
+    '/deterministic'
+    '/optimize'
+    '/nologo'
+    '/platform:anycpu'
+    '/target:exe'
+    "/out:""$dist\shim.exe"""
+    "$src\shim.cs"
+
+)
+& $csc @shimCS
 
 # Checksums
 Write-Output 'Computing checksums ...'
-Get-ChildItem "$build\*" -Include *.exe,*.dll -Recurse | ForEach-Object {
-    $checksum = (Get-FileHash -Path $_.FullName -Algorithm SHA256).Hash.ToLower()
-    "$checksum *$($_.Name)" | Tee-Object -FilePath "$build\$($_.Name).sha256" -Append
+Get-ChildItem "$dist\*" -Include '*.exe' -Recurse | ForEach-Object {
+    $checksum = (Get-FileHash -Path $_.FullName -Algorithm 'SHA256').Hash.ToLower()
+    "$checksum *$($_.Name)" | Tee-Object -FilePath "$dist\$($_.Name).sha256" -Append
 }
 
 Pop-Location
